a20fb6dd09efbcb0b5242fead9973979cfe4907e
RevengerCode <98663438+RevengerCode@users.noreply.github.com>
2025-02-10 16:31:13 +0100

    css navbar e logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Login from './pages/Login'
 import NavBar from './components/NavBar'
 import { useLocation } from 'react-router-dom';
 import { useEffect,useState } from 'react';
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,87 +1,84 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import  { useContext } from 'react';
-import { Link, useNavigate } from "react-router-dom";
+import React from 'react';
+import { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext'; // Importa il contesto
 import './NavBar.css';  // Importa il file CSS
+import logo from '../assets/logo.svg'; // Assicurati di avere il logo in questa directory
 
-function NavBar(){
+function NavBar() {
+  const navigate = useNavigate();
+  const { user, logout } = useContext(AuthContext);
 
-    const navigate = useNavigate();
-    const { user, logout } = useContext(AuthContext); // Usa il contesto per ottenere l'utente
+  const isLoggedIn = !!user;
 
-    // Controlla se il token è presente in localStorage
-    const isLoggedIn = !!user; // Verifica se l'utente è loggato
+  const handleLogout = () => {
+    logout();
+    navigate('/', { state: { message: 'Logout successful!' } });
+  };
 
-  
-    // Funzione per il logout
-    const handleLogout = () => {
-      logout(); //Elimino il contesto e il localStorage
-      navigate('/',{ state: { message: 'Logout successful!' } }); // Reindirizza alla home page
-    };
-  
-    return (
-        <nav className="navbar">
-          <span className="navbar-container">
-            <Link to="/" className="navbar-link">
-              <button className="navbar-button">HOME</button>
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        {/* Logo a sinistra */}
+        <Link to="/" className="logo-container">
+          <img src={logo} alt="Logo" className="navbar-logo" />
+        </Link>
+
+        {/* Link della navbar */}
+        <div className="navbar-links">
+          <Link to="/" className="navbar-link">
+            <button className="navbar-button">HOME</button>
+          </Link>
+          {isLoggedIn ? (
+            user.role === 'utente' ? (
+              <div className="navbar-links-utente">
+                <Link to="/prenotazioni" className="navbar-link">
+                  <button className="navbar-button">Prenotazioni</button>
+                </Link>
+                <Link to="/schede" className="navbar-link">
+                  <button className="navbar-button">Schede</button>
+                </Link>
+                <Link to="/anagrafica" className="navbar-link">
+                  <button className="navbar-button">Anagrafica</button>
+                </Link>
+                <Link to="/richiesta-scheda" className="navbar-link">
+                  <button className="navbar-button">Richiesta</button>
+                </Link>
+                <button className="navbar-button navbar-button-logout" onClick={handleLogout}>
+                  Logout
+                </button>
+              </div>
+            ) : user.role === 'pt' ? (
+              <div className="navbar-links-pt">
+                <Link to="/gestione-richieste" className="navbar-link">
+                  <button className="navbar-button">Gestisci Richieste</button>
+                </Link>
+                <button className="navbar-button navbar-button-logout" onClick={handleLogout}>
+                  Logout
+                </button>
+              </div>
+            ) : user.role === 'admin' ? (
+              <div className="navbar-links-admin">
+                <Link to="/gestione-utenti" className="navbar-link">
+                  <button className="navbar-button">Gestisci Utenti</button>
+                </Link>
+                <Link to="/gestione-pt" className="navbar-link">
+                  <button className="navbar-button">Gestisci Pt</button>
+                </Link>
+                <button className="navbar-button navbar-button-logout" onClick={handleLogout}>
+                  Logout
+                </button>
+              </div>
+            ) : null
+          ) : (
+            <Link to="/login" className="navbar-link">
+              <button className="navbar-button">LOGIN</button>
             </Link>
-    
-            {/* Se l'utente è loggato, mostriamo i bottoni per il logout e altre pagine */}
-            {isLoggedIn ? (
-              <span className="navbar-links">
-                {// Se ha il ruolo di utente, mostriamo i bottoni della sua area personale
-                  user.role === 'utente' ? (
-                    <span className="navbar-links-utente">
-                        <Link to="/prenotazioni" className="navbar-link">
-                            <button className="navbar-button">Prenotazioni</button>
-                        </Link>
-                        <Link to="/schede" className="navbar-link">
-                            <button className="navbar-button">Schede</button>
-                        </Link>
-                        <Link to="/anagrafica" className="navbar-link">
-                            <button className="navbar-button">Anagrafica</button>
-                        </Link>
-                        <Link to="/richiesta-scheda" className="navbar-link">
-                            <button className="navbar-button">Richiesta</button>
-                        </Link>
-                        <button className="navbar-button" onClick={handleLogout}>
-                         Logout
-                        </button>
-                    </span>
-                  /* Se ha il ruolo di pt, mostriamo i bottoni della sua area personale */
-                ) : user.role === 'pt' ? (
-                    <span className="navbar-links-pt">
-                        <Link to="/gestione-richieste" className="navbar-link">
-                            <button className="navbar-button">Gestisci Richieste</button>
-                        </Link>
-                        <button className="navbar-button" onClick={handleLogout}>
-                          Logout
-                         </button>
-                    </span>
-                ) : user.role === 'admin' ? (
-                  <span className="navbar-links-admin">
-                      <Link to="/gestione-utenti" className="navbar-link">
-                          <button className="navbar-button">Gestisci Utenti</button>
-                      </Link>
-                      <Link to="/gestione-pt" className="navbar-link">
-                          <button className="navbar-button">Gestisci Pt</button>
-                      </Link>
-                      <button className="navbar-button" onClick={handleLogout}>
-                        Logout
-                       </button>
-                  </span>
-                ) : null}
-              </span>
-              //Se non è loggato, mostriamo il bottone per  il Login
-            ) : (
-              <Link to="/login" className="navbar-link">
-                <button className="navbar-button">LOGIN</button>
-              </Link>
-            )}
-          </span>
-        </nav>
-      );
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
